ed93c740f6d658b712a6a6e1a46cc6fb8d82d21f
Zhenkai Liang <liangzk@Mac.linksys.cache>
2025-11-01 07:43:22 +0800

    update

--- copyfile.sh
+++ copyfile.sh
@@ -1,2 +1,8 @@
-echo cp ../zhenkai-liang.github.io/_site/$1/index.html $1/index.html
-cp ../zhenkai-liang.github.io/_site/$1/index.html $1/index.html
+for i in . research publication teaching cv hobby
+do
+  echo cp ../zhenkai-liang.github.io/_site/$i/index.html $i/index.html
+  cp ../zhenkai-liang.github.io/_site/$i/index.html $i/index.html
+done
+
+
+#cp ../zhenkai-liang.github.io/_site/$1/index.html $1/index.html
