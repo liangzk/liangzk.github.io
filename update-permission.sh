68e1dd7d257f574b4e1fb51340c31d344632a84b
LIANG Zhenkai <liangzk@comp.nus.edu.sg>
2025-11-01 12:10:34 +0800

    publication

--- update-permission.sh
+++ update-permission.sh
@@ -1,5 +1,10 @@
-git pull
+#git pull
 chmod a+r *.html
 chmod a+r styles.css
 chmod a+r *.js
 chmod a+r papers/*
+chmod o+x assets
+chmod o+x assets/*
+chmod a+r assets/css/* assets/fonts/* assets/js/* assets/webfonts/*
+chmod o+x research publications teaching cv hobby images 
+chmod a+r research/* publications/* teaching/* cv/* hobby/* images/*
